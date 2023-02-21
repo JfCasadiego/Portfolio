b337fe6bf1297aae409a0022fa04bcc5630c0ba3
Jose Fernando Casadiego Mora <jose.fernando.casadiego@gmail.com>
2023-02-20 19:31:07 -0500

    terminado los estilos para la web, pendiente acerla responsive

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,8 @@
 const cards = document.querySelectorAll('.card');
 const nav = document.querySelector('.navigation-bar');
+const textSecont = document.querySelector(".writing-second-words")
+
+
 let lastScrollPos = window.pageYOffset;   
 
 cards.forEach(card =>{
@@ -7,29 +10,43 @@ cards.forEach(card =>{
 
         const x =  e.pageX - card.offsetLeft;
         const y =  e.pageY - card.offsetTop;
-
-
-
         card.style.setProperty('--x', x + 'px');
         card.style.setProperty('--y', y + 'px');
-        card.style.color="white"
+        
     })
+
+    card.addEventListener('mouseenter', function(e){
+        card.style.color="white"       
+    })
+
+
     card.addEventListener('mouseleave', function(e){
         card.style.color="black"
     })
 })
 
-
 window.addEventListener("scroll", function(e){
-     
-    
+
     if (window.pageYOffset < lastScrollPos) {
-        nav.style.height="3rem"
+        nav.style.top="0px"
       } else {
-        nav.style.height="0rem"
+        nav.style.top="-3rem"
       }
     
-    lastScrollPos = window.pageYOffset; 
-  
-    
+    lastScrollPos = window.pageYOffset;   
 });
+
+const textLoad = () => {
+    let delay = 0;
+  
+    const texts = [    "Freelancer.",    "Front-End developer.",    "Back-End developer.",    "Full-Stack developer.",  ];
+  
+    texts.forEach((text, index) => {
+      setTimeout(() => {
+        textSecont.textContent = text;
+      }, delay + index * 4000);
+    });
+  };
+  
+  textLoad();
+  setInterval(textLoad, 16000);
